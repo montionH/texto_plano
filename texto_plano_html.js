a07c57c4a315533997379d2f6f271303c2b01a0f
montionH <66965471+montionH@users.noreply.github.com>
2021-11-04 21:33:27 -0700

    Add files via upload
    creación de servidor empleando código de html

---
+++ texto_plano_html.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    //res.setHeader('Context-Type', 'text/plain');
+    res.setHeader('Context-Type', 'text/html');
+    res.end('<h1>hola mundo\n</h1>');
+});
+server.listen(port, hostname, () => {
+    console.log('el servidor se esta ejecutando en http://127.0.0.1:3000/'); //${hostname}:${port}
+});
